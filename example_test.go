26a15e1a1e0c48e673f553f33bdf85912244cec6
Hiroaki Nakamura <hnakamur@gmail.com>
2017-10-05 12:51:29 +0900

    Add an example test just for showing an example, not for test

---
+++ example_test.go
@@ -0,0 +1,38 @@
+package contextify_test
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/hnakamur/contextify"
+)
+
+func ExampleContextify() {
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+
+		s := <-c
+		log.Printf("received signal, %s", s)
+		cancel()
+		log.Printf("cancelled context")
+	}()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		w.Write([]byte("Hello, example http server\n"))
+	})
+	s := http.Server{Addr: ":8080"}
+	err := contextify.Contextify(func() error {
+		return s.ListenAndServe()
+	}, func() error {
+		return s.Shutdown(context.Background())
+	}, nil)(ctx)
+	if err != nil {
+		log.Printf("got error, %v", err)
+	}
+	log.Print("exiting")
+}
